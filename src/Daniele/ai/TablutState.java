50efb6ff4d3bc4ebb43e7a2602e9bacd2138ea7a
Matteo Castiglio <matteo.castiglii@studio.unibo.it>
2019-05-09 20:30:03 +0200

    Galassi bugfix#7-2

--- src/Daniele/ai/TablutState.java
+++ src/Daniele/ai/TablutState.java
@@ -458,7 +458,7 @@ private State checkCaptureBlackKingDown(State state, DanieleAction a){
 	// ho il re sotto
 	if (a.getRowTo() < state.getBoard().length - 2
 			&& state.getPawn(a.getRowTo() + 1, a.getColumnTo()).equalsPawn("K")) {
-		System.out.println("Ho il re sotto");
+	
 		// re sul trono
 		if (state.getBox(a.getRowTo() + 1, a.getColumnTo()).equals("e5")) {
 			if (state.getPawn(5, 4).equalsPawn("B") && state.getPawn(4, 5).equalsPawn("B")
